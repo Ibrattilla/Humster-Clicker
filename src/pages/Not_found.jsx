cef8df25c3c55df075bc98e9182d3589b119ad8d
Ibrattilla <ibrattillaravshanov@gmail.com>
2026-05-03 18:44:17 +0500

    fix

--- src/pages/Not_found.jsx
+++ src/pages/Not_found.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../App.css"
+import song from "../assets/Humster song.mp4"
 
 const Not_found = () => {
   return (
@@ -7,6 +8,7 @@ const Not_found = () => {
       <h1 className='Eror'>Eror 404</h1>
       <h2 className='eror'>You may listen this like entertaiment</h2>
       <iframe style={{marginLeft:"50px"}} width="860" height="615" src="https://www.youtube.com/embed/H9K8-3PHZOU?si=DN7m79bPzpRSBGFW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <video src={song}></video>
     </div>
   )
 }
